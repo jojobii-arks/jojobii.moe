58236750806a1e3b58ee549923d153a828e36e6e
jojobii-arks <jojobii.arks@gmail.com>
2023-09-21 03:46:44 -0700

    update

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,7 @@
 export const links: { label: string, sub?: string, url: string}[] = [
   { label: 'Fediverse', url: 'https://mk.arks.cafe/@jojobii'},
   { label: 'Twitter', url: 'https://twitter.com/jojobii_'},
+  { label: 'TikTok', url: 'https://www.tiktok.com/@jojobii_'},
   { label: 'SoundCloud', url: 'https://soundcloud.com/jojobii'},
   { label: 'Spotify', url: 'https://open.spotify.com/artist/7AceGacY9eupr9q5MIoR8L?si=Kb5zsnWGTEmuF2eFL___IQ'},
   { label: 'GitHub', url: 'https://github.com/jojobii-arks'},
